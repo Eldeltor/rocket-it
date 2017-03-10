b2faa305ebb89f5ad7fc7e3c0152403b1da59b47
Eldeltor <guido_botta@hotmail.com>
2017-03-10 04:53:13 -0300

    rocket

--- js/rocket.js
+++ js/rocket.js
@@ -3,11 +3,35 @@ var countdownNumber = 10;
 
 var changeState = function (state) {
 	document.body.className = 'body-state'+state;
+	clearInterval(timer);
+	countdownNumber = 10;
+	document.getElementById('countdown').innerHTML = countdownNumber;
 
+	//countdown
 	if (state == 2) {
 		timer = setInterval(function (){
-			document.getElementById('countdown').innerHTML = countdownNumber;
 			countdownNumber = countdownNumber -1;
+			document.getElementById('countdown').innerHTML = countdownNumber;
+			
+			if (countdownNumber <=-1) {
+			changeState(3);
+			};
 		}, 1000);
+	//success or failure
+	} else if (state == 3) {
+		var success = setTimeout(function (){
+			var randomNumber = Math.round (Math.random ()*10);
+
+			console.log('randomNumber:',randomNumber)
+
+			// success
+			if (randomNumber > 5) {
+				changeState(4)
+			}
+			//failure 
+			else {
+				changeState(5);
+			}
+		}, 2000);
 	};
 }
